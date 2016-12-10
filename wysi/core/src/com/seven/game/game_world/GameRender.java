3049694c509384853f1c4125194fb9130a4ad21b
Tsegelnikov Ruslan <hanstrus@gmail.com>
2016-12-10 16:57:12 +0600

    add gameWorld and gameRendering

---
+++ wysi/core/src/com/seven/game/game_world/GameRender.java
@@ -0,0 +1,40 @@
+package com.seven.game.game_world;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.seven.game.utils.AssetLoader;
+
+public class GameRender {
+    private OrthographicCamera camera;
+    private ShapeRenderer shapeRenderer;
+    private SpriteBatch batcher;
+    private GameWorld myWorld;
+
+    public GameRender(GameWorld gameWorld) {
+        myWorld = gameWorld;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(true, 100, 200);
+
+        batcher = new SpriteBatch();
+        batcher.setProjectionMatrix(camera.combined);
+
+        shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setProjectionMatrix(camera.combined);
+    }
+
+    public void render(float runTime) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+        batcher.begin();
+        batcher.disableBlending();
+        batcher.draw(AssetLoader.carpet, 0, 0, 400, 600);
+        batcher.enableBlending();
+        batcher.end();
+    }
+}
---
+++ wysi/core/src/com/seven/game/game_world/GameWorld.java
@@ -0,0 +1,11 @@
+package com.seven.game.game_world;
+
+public class GameWorld {
+    public GameWorld() {
+
+    }
+
+    public void update(float delta) {
+
+    }
+}
